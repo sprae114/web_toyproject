12e0feeda09cd2513f7675a9fca24e50c7cf9003
KangYoHan <dygks34@gmail.com>
2022-02-18 07:57:10 +0900

    Initial commit

---
+++ demo/src/main/java/webtoy/demo/web/IndexController.java
@@ -0,0 +1,45 @@
+package webtoy.demo.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import webtoy.demo.config.auth.LoginUser;
+import webtoy.demo.config.auth.dto.SessionUser;
+import webtoy.demo.web.dto.PostsResponseDto;
+import webtoy.demo.service.PostsService;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+    private final HttpSession httpSession;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave(){
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("post",dto);
+        return "posts-update";
+    }
+}
---
+++ demo/src/main/java/webtoy/demo/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package webtoy.demo.web.dto;
+import webtoy.demo.domain.posts.Posts;
+import lombok.Getter;
+
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
